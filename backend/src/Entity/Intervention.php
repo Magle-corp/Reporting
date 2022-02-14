db21f5df81be5b6ac90fea643948fc5166a35168
Magle-corp <wolff.kevin@protonmail.com>
2022-02-14 16:20:37 +0100

    Fix entity Intervention use statement

--- backend/src/Entity/Intervention.php
+++ backend/src/Entity/Intervention.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\InterventionRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 use Knp\DoctrineBehaviors\Contract\Entity\TimestampableInterface;
 use Knp\DoctrineBehaviors\Model\Timestampable\TimestampableTrait;
