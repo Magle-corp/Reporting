95464f1b2d036192f468b79c85dfd986f16df743
Magle-corp <wolff.kevin@protonmail.com>
2022-02-18 21:56:11 +0100

    Use React with Typescript

---
+++ frontend/src/theme/color.ts
@@ -0,0 +1,6 @@
+const color: object = {
+  black: '#222222',
+  white: '#ffffff',
+};
+
+export default color;
---
+++ frontend/src/ui/Layout.tsx
@@ -0,0 +1,24 @@
+// Use.
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  className?: string;
+  children: ReactNode;
+  variant?: string;
+}
+
+const StyledLayout = styled.div<{ variant?: string }>``;
+
+/**
+ * Provide ui component Layout.
+ */
+const Layout = ({ className, children, variant, ...props }: Props) => {
+  return (
+    <StyledLayout className={className} variant={variant} {...props}>
+      {children}
+    </StyledLayout>
+  );
+};
+
+export { Layout };
